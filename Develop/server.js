7aa18504a5e22f27cdf50a84a4604258736a559a
Joshua Kennedy-Dedam <joshuajordyn@hotmail.com>
2023-11-07 08:05:27 -0800

    Got the req.body to be parsed, wasnt using app.use(express.json())

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3001;
 const file = require("../Develop/db/db.json");
 app.use(express.static("public"));
-
+app.use(express.json());
 //This is basically like boiler plate server stuff
 
 //this is for the json api notes
@@ -35,6 +35,15 @@ app.get('/api/notes/:id' , (req,res) => {
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "../Develop/public/notes.html"))
 );
+
+
+app.post('/api/notes' , (req,res) => {
+    console.info(`${req.method} request received to add a review`);
+    console.info(req.body);
+   
+    const {id,title,text} = req.body;
+    
+})
 
 //this is just something that triggers when the server gets running.
 app.listen(PORT, () => console.log("server is listening!"));
